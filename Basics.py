1c1f9a3d5f930ef3173a8a41642f40e73b6ba5b7
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-05-15 14:41:40 +0530

    Python Basic Workshop Day1

---
+++ Basics.py
@@ -0,0 +1,9 @@
+# print("Hello World") 
+# first_Variable = input("Enter a name: ");
+# second_Variable = float(input("Enter a number: \n"));
+# print(f"{first_Variable} and floating value is {second_Variable}");
+
+name= "Rahul", "Raj", "Rohan",78,55.8
+name1=["Rahul", "Raj", "Rohan",78,55.8]
+name1[3]= "a"
+print(name1)
